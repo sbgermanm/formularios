beb65ece2efc0586d04e648d5d8a5c7c9a1a6b14
sebas portatil 2013 <sgerman@indra.es>
2014-11-12 09:43:24 +0100

    desplegable curso niño

--- src/main/java/es/sebas/formularios/Controller/FormulariosController.java
+++ src/main/java/es/sebas/formularios/Controller/FormulariosController.java
@@ -1,15 +1,24 @@
 package es.sebas.formularios.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.sebas.formularios.Entity.Hoc;
 
 @Controller
 public class FormulariosController {
 
-	
-	@RequestMapping("/hoc")
-	public String showHocForm(){
-		return "hoc-form";
+	@ModelAttribute("hoc")
+	public Hoc contructHoc() {
+		return new Hoc();
 	}
-	
+
+	@RequestMapping("/hoc")
+	public String showHocForm() {
+		System.out.println("entró");
+
+		return "hocform";
+	}
+
 }
